4e39f5351e6e2bea8392cd7e64236b43e82c8743
sanjay.khatri <sanjay.khatri@sbsolutionsnepal.com>
2023-05-08 21:44:30 +0545

    Retrofit client

---
+++ app/src/main/java/com/example/notebookapp/quotes/api/RetrofitClient.java
@@ -0,0 +1,17 @@
+package com.example.notebookapp.quotes.api;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class RetrofitClient {
+
+    private static Retrofit retrofit;
+    private static String baseUrl = "https://zenquotes.io/api/";
+
+    public static Retrofit getRetrofit() {
+        if (retrofit == null) {
+            retrofit = new Retrofit.Builder().baseUrl(baseUrl).addConverterFactory(GsonConverterFactory.create()).build();
+        }
+        return retrofit;
+    }
+}
